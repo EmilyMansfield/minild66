33e043ba1d70911df19e1cf31564e6bb829fe1a6
Daniel Mansfield <daniel.mansfield99@gmail.com>
2016-03-27 15:35:23 +0100

    Added proper point contain function for convex polygons

--- src/navmesh.hpp
+++ src/navmesh.hpp
@@ -15,21 +15,23 @@ public:
     // First point must be ac of the positive horizontal
     std::vector<sf::Vector2f> points;
 
-    // Points are defined anticlockwise, so the angle of a ray
-    // from a point to each point in that order will increase
-    // iff the point lies in the polygon
+    // Loop through the points in anticlockwise order and draw
+    // a vector between consecutive points. If the points is
+    // left of every vector, it's in the polygon
     bool contains(const sf::Vector2f& point)
     {
-        float prev_theta = 0.0f;
-        for(auto p : points)
+        for(int i = 0; i < points.size(); ++i)
         {
-            auto v = p - point;
-            float norm = vecmath::norm(v);
-            float theta = std::asin(v.y / norm);
-            if(theta < 0) theta += 2*vecmath::pi;
-            if(theta < prevTheta) return false;
-            prev_theta = theta;
+            auto p1 = points[i];
+            auto p2 = points[(i+1)%points.size()];
+            // To check the side of the line AB, take AB x AX
+            // and look at the sign. If +ve, by the right hand rule
+            // the point X is to the left of AB
+            auto v = p2 - p1;
+            auto u = point - p1;
+            if(v.x*u.y - v.y*u.x < 0) return false;
         }
+
         return true;
     }
 };
